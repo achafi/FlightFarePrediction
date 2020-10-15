56fadd5fdad8379b7684ea39b3106b6ed8470751
achafi <assia.chafii@hotmail.com>
2020-10-15 19:10:28 +0200

    add app.py and templates

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+from flask_cors import cross_origin
+import sklearn
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open("flight_rf.pkl", "rb"))
+
+
+@app.route("/")
+@cross_origin()
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
